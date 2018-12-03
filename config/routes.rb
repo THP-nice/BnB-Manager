ee631f251ddab1bf932a9c2c8ac3a042f0192780
Natdenice <Clad1987@hotmail.com>
2018-12-03 16:13:26 +0100

    Creating static_pages controller and views related + configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'static_pages#home'
+get 'about', to: "static_pages#about"
+get 'contact', to: "static_pages#contact"
+get 'customers_reviews', to: "static_pages#customers_reviews"
+get 'services', to: "static_pages#services" 
+  
 end
