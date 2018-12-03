898678c70e4f797f1d4c877542399f14057b858d
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-03 16:48:32 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
 
-devise_for :users
+  devise_for :users
 
-root 'static_pages#home'
-get 'about', to: "static_pages#about"
-get 'contact', to: "static_pages#contact"
-get 'customers_reviews', to: "static_pages#customers_reviews"
-get 'services', to: "static_pages#services" 
+  get 'about', to: "static_pages#about"
+  get 'contact', to: "static_pages#contact"
+  get 'customers_reviews', to: "static_pages#customers_reviews"
+  get 'services', to: "static_pages#services" 
   
 end
