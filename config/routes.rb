485673c5c2dcee55f95c678f17e25fdd9cfa2909
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-03 17:28:42 +0100

    services route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about', to: "static_pages#about"
   get 'contact', to: "static_pages#contact"
+  get 'services', to: "static_pages#services"
   get 'prices', to: "static_pages#prices"
   get 'packs', to: "static_pages#packs"
   get 'faq', to: "static_pages#faq"
