3221248376a15c58d3c48f0deb2f250bdde6e109
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-04 04:48:47 +0100

    devise permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if:  :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthday, :phone_number])
+      added_attrs = [:first_name, :last_name, :email, :password, :password_confirmation, :birthday, :phone_number]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
     end
 
 end
