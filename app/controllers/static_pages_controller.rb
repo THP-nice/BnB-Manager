5e0e2087123a94f6dada3e74b3b98ff4f3fa40af
Ysalien <enilasy.m@gmail.com>
2018-12-04 14:21:23 +0100

    remove request mailchimp from controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
-    # Definition de base du mailer - à mettre ailleurs ou pas,
-    # je ne sais pas encore où les users s'inscrivent
-    @list_id = Rails.application.credentials.dig(:gibbonlistid)
-    gibbon = Gibbon::Request.new
 
-   gibbon.lists(@list_id).members.create(
-     body: { email_address: params[:email][:address],
-             status: "Subscribed"
-     }
-   )
-
-   respond_to do |format|
-     format.json{render :json => { :message => "Email de confirmation envoyé !" }}
-       end
   end
 
   def about
