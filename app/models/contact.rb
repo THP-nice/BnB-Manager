a68bc205e3edb6af1300b2dc54dc015f9c5af8a7
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-11 20:12:33 +0100

    check the presence of phone_number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact
   include ActiveModel::Model #Permet d'utiliser le model et ses attributs sans ActiveRecord
-  attr_accessor :name, :email, :body
+  attr_accessor :name, :email, :body, :phone_number
   validates :name, :email, :body, presence: true
 end
