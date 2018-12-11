992748932ab72ecb3e54b597f716ff81decdba80
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-11 20:10:51 +0100

    add phone_number attr_accessor

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,16 +4,30 @@ class ContactMailer < ApplicationMailer
 
   def contact_email(contact)
     @contact = contact
-    mail(
-      from: @contact.email,
-      to: "Bnbmanager.infos@gmail.com",
-      name: @contact.name,
-      email: @contact.email,
-      subject: "Nouveau message de #{@contact.name}",
-      body: "Demande de la part de #{@contact.name}.
-      #{@contact.body}.
-      Contact : #{@contact.email}."
-    )
-  end
 
+    if @contact.phone_number.present?
+      mail(
+        from: @contact.email,
+        to: "Bnbmanager.infos@gmail.com",
+        name: @contact.name,
+        email: @contact.email,
+        subject: "Nouveau message de #{@contact.name}",
+        body: "Demande de la part de #{@contact.name}.
+        #{@contact.body}.
+        Contact : #{@contact.email}
+        Téléphone : #{@contact.phone_number}"
+      )
+    else
+      mail(
+        from: @contact.email,
+        to: "Bnbmanager.infos@gmail.com",
+        name: @contact.name,
+        email: @contact.email,
+        subject: "Nouveau message de #{@contact.name}",
+        body: "Demande de la part de #{@contact.name}.
+        #{@contact.body}.
+        Contact : #{@contact.email}"
+      )
+    end
+  end
 end
