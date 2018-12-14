f5117f80191a7d444d86746e31b88d4ea5bacda4
Nikita Vasilev <vassilev.nikita@gmail.com>
2018-12-14 18:47:14 +0100

    delete of validates_with IbanValidator in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :properties
 
   after_create :subscribe
-  validates_with IbanValidator, on: :update
 
   def subscribe
     @list_id = Rails.application.credentials.dig(:gibbonlistid)
