14680df0cff89a9c0a93b6c5db370450a8e05c23
Ysalien <enilasy.m@gmail.com>
2018-12-16 17:01:10 +0100

    add views for Mon compte bancaire

--- app/controllers/iban_controller.rb
+++ app/controllers/iban_controller.rb
@@ -8,17 +8,17 @@ class IbanController < ApplicationController
   def create
     @iban = Iban.new(iban_params)
       if @iban.save
-        redirect_to edit_user_registration_path, notice: 'Iban enregistré.'
+        redirect_to iban_index_path, notice: 'Iban enregistré.'
       else
         render :new, notice: 'Une erreur est survenue lors de la saisie, veuillez réessayer.'
       end
   end
 
-  def show
-    if current_user.id == Iban.find(params[:id]).user_id
-      @iban = Iban.find(params[:id])
+  def index
+    if current_user.id == Iban.last.user_id
+      @iban = Iban.last
     else
-      redirect_to root_path, notice: "Oups, cet accès vous est interdit."
+      redirect_to iban_index_path, notice: "Oups, cet accès vous est interdit."
     end
   end
 
@@ -26,14 +26,14 @@ class IbanController < ApplicationController
     if current_user.id == Iban.find(params[:id]).user_id
       @iban = Iban.find(params[:id])
     else
-      redirect_to edit_user_registration_path, notice: "Oups, cet accès vous est interdit."
+      redirect_to iban_index_path, notice: "Oups, cet accès vous est interdit."
     end
   end
 
   def update
     @iban = Iban.find(params[:id])
       if @iban.update(iban_params)
-        redirect_to edit_user_registration_path, notice: 'Iban enregistré.'
+        redirect_to iban_index_path, notice: 'Iban enregistré.'
       else
         render :edit, notice: 'Une erreur est survenue lors de la saisie, veuillez réessayer.'
       end
